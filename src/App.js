40000a905cee8f20264e519ba2ea499e234325b6
Fernando Britto <me@fernandobritto.com.br>
2020-03-03 22:17:38 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Router } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+
 import history from './services/history'
 import GlobalStyle from './styles/GlobalStyles'
 import Header from './components/Header'
@@ -11,6 +13,7 @@ function App() {
       <Header />
       <Routes />
       <GlobalStyle />
+      <ToastContainer autoClose={3000} className="toast-constainer" />
     </Router>
   )
 }
