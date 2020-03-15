dd2b33ca1ef2e8c9e165f831e099f29afa3b6d21
Fernando Britto <me@fernandobritto.com.br>
2020-03-15 09:25:37 -0300

    Initial commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from 'react'
+import api from '../../services/api'
 
 export default function Home() {
   const [trips, setTrips] = useState([])
-  useEffect(() => {}, [])
+  useEffect(() => {
+    async function loadApi(){
+      const response = await api.get('trips')
+      setTrips(response.data)
+
+    }
+
+    loadApi()
+
+  }, [])
 
   return (
-    <div>Home</div>
+    <div>
+      <div className="box">
+          {trips.map(trip =>(
+            <li key={trip.id}>
+              <strong>{trip.title}</strong>
+          <span>Status: {trip.status ? 'Disponoval':'Indisponivel'}</span>
+            </li>
+          ))}
+      </div>
+    </div>
   )
 }
