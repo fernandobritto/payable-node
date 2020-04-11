5beb435e7822c3ec56c06e0fb9bcac1266973b14
Fernando Britto <me@fernandobritto.com.br>
2020-04-11 07:14:22 -0300

    Initial commit

--- database/migrations/2020_04_10_163901_create_table_real_state.php
+++ database/migrations/2020_04_10_163901_create_table_real_state.php
@@ -15,6 +15,7 @@ class CreateTableRealState extends Migration
     {
         Schema::create('real_state', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->string('description');
             $table->text('content');
@@ -26,6 +27,8 @@ class CreateTableRealState extends Migration
             $table->string('slug');
 
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
