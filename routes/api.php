9dcd197adc0e2d6b0c63e5b1df65f071f379cdc1
Fernando Britto <me@fernandobritto.com.br>
2020-04-23 11:15:19 -0300

    Initial commit

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::prefix('v1')->namespace('Api')->group(function(){
-    Route::prefix('real-states')->name('real_states.')->group(function(){
-        Route::resource('/', 'RealStateController');
+    Route::name('real_states.')->group(function(){
+        Route::resource('real-states', 'RealStateController');
     });
 });
